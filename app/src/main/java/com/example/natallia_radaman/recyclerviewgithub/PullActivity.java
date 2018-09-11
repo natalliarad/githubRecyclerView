4127014422cf4e3a8557f9a800d11aaf38eb5b56
Natallia Radaman <Natallia_Radaman@epam.com>
2018-09-11 14:18:34 +0300

    Initialization of the Repository

---
+++ app/src/main/java/com/example/natallia_radaman/recyclerviewgithub/PullActivity.java
@@ -0,0 +1,13 @@
+package com.example.natallia_radaman.recyclerviewgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PullActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pull2);
+    }
+}
